224d31a0e8fd58b95f8191fd8988ac8b778f08fd
truedigitalhustle-debug <truedigitalhustle@gmail.com>
2026-07-26 14:27:23 -0700

    Add MagicaBonus component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,24 +2,26 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import SocialProof from "@/components/SocialProof";
 import WhatYoullGet from "@/components/WhatYoullGet";
+import MagicaBonus from "@/components/MagicaBonus";
 import WhyItWorks from "@/components/WhyItWorks";
 import FAQ from "@/components/FAQ";
 import FinalCTA from "@/components/FinalCTA";
 import Footer from "@/components/Footer";
 
 export default function Home() {
-  return (
-    <div className="relative flex min-h-screen flex-col overflow-hidden">
-    <Header />
-    <main className="flex-1">
-    <Hero />
-    <SocialProof />
-    <WhatYoullGet />
-    <WhyItWorks />
-    <FAQ />
-    <FinalCTA />
-    </main>
-    <Footer />
-    </div>
-    );
+    return (
+          <div className="relative flex min-h-screen flex-col overflow-hidden">
+                <Header />
+                <main className="flex-1">
+                        <Hero />
+                        <SocialProof />
+                        <WhatYoullGet />
+                        <MagicaBonus />
+                        <WhyItWorks />
+                        <FAQ />
+                        <FinalCTA />
+                </main>
+                <Footer />
+          </div>
+        );
 }
